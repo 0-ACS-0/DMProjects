c3d09259803e27b5b4c355b0550d72c6e92ab919
Antonio <140327781+0-ACS-0@users.noreply.github.com>
2025-09-25 23:27:12 +0200

    Delete dmmsg directory

--- dmmsg/sside/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -I./libs/dmserver -I./libs/dmlogger \
- dmmsg_sside.c \
- -L./libs/dmserver -L./libs/dmlogger \
- -ldmserver -ldmlogger \
- -Wl,-rpath=$(pwd)/libs/dmserver \
- -Wl,-rpath=$(pwd)/libs/dmlogger \
- -o dmmsg_server.elf
